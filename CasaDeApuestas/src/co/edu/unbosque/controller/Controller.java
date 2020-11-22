b2ee5028194e1214967abe2a9742ea44c1ca3d4b
juan <juan@localhost>
2020-11-21 23:27:39 -0500

    Se añade el panel de bienvenida y de creación de casa y sedes

--- CasaDeApuestas/src/co/edu/unbosque/controller/Controller.java
+++ CasaDeApuestas/src/co/edu/unbosque/controller/Controller.java
@@ -1,5 +1,13 @@
 package co.edu.unbosque.controller;
 
+import co.edu.unbosque.view.View;
+
 public class Controller {
 
+	private View gui;
+	
+	public Controller() {
+		
+	    gui = new View();
+	}
 }
